66115d719d9f26d2d18602dfa36f3abad89fcdd1
estherbreath <estherojile248@gmail.com>
2023-01-31 08:08:22 +0100

    changes

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	// "log"
 	// "os"
-	"reflect"
-	"strconv"
+	// "reflect"
+	// "strconv"
 )
 
 var pl = fmt.Println
@@ -37,28 +37,42 @@ func main() {
 	// pl(reflect.TypeOf("Hello"))
 	// pl(reflect.TypeOf('🚓'))
 
-	//Casting
-	cV1 := 1.5
-	cV2 := int(cV1)
-	pl(cV2)
+	// //Casting
+	// cV1 := 1.5
+	// cV2 := int(cV1)
+	// pl(cV2)
 
-	//String to int
-	cV3 := "5000000"
-	cV4, err := strconv.Atoi(cV3)
-	pl(cV4, err, reflect.TypeOf(cV4))
+	// //String to int
+	// cV3 := "5000000"
+	// cV4, err := strconv.Atoi(cV3)
+	// pl(cV4, err, reflect.TypeOf(cV4))
 
-	//Int to string
-	cV5 := 5000000
-	cV6 := strconv.Itoa(cV5)
-	pl(cV6)
+	// //Int to string
+	// cV5 := 5000000
+	// cV6 := strconv.Itoa(cV5)
+	// pl(cV6)
 
-	//String to float 
-	cV7 := "3.14"
-	if cV8, err := strconv.ParseFloat(cV7, 64); err == nil{
-		pl(cV8)
+	// //String to float 
+	// cV7 := "3.14"
+	// if cV8, err := strconv.ParseFloat(cV7, 64); err == nil{
+	// 	pl(cV8)
+	// }
+
+	//IF STATEMENT
+	//Conditional Operators : < > >= <= == !=
+	//Logical Operators : && || !
+
+	iAge := 8
+	if (iAge >= 1) && (iAge <= 18){
+		pl("Important Birthday")
+	}else if (iAge == 21) || (iAge == 50){
+		pl("Important Birthday")
+	}else if iAge >= 65 {
+		pl("Important Birthday")
+	}else{
+		pl("Not an Important Birthday")
 	}
-
-
-	
+	pl("!true =", !true)
+	 
 }
 
