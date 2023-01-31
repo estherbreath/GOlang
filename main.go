cdc0b9423a0717fdc07da5e7d9205acb00975c9b
estherbreath <estherojile248@gmail.com>
2023-01-31 09:04:59 +0100

    changes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	// "bufio"
 	"fmt"
+	"strings"
 	// "log"
 	// "os"
 	// "reflect"
@@ -62,17 +63,35 @@ func main() {
 	//Conditional Operators : < > >= <= == !=
 	//Logical Operators : && || !
 
-	iAge := 8
-	if (iAge >= 1) && (iAge <= 18){
-		pl("Important Birthday")
-	}else if (iAge == 21) || (iAge == 50){
-		pl("Important Birthday")
-	}else if iAge >= 65 {
-		pl("Important Birthday")
-	}else{
-		pl("Not an Important Birthday")
-	}
-	pl("!true =", !true)
+	// iAge := 8
+	// if (iAge >= 1) && (iAge <= 18){
+	// 	pl("Important Birthday")
+	// }else if (iAge == 21) || (iAge == 50){
+	// 	pl("Important Birthday")
+	// }else if iAge >= 65 {
+	// 	pl("Important Birthday")
+	// }else{
+	// 	pl("Not an Important Birthday")
+	// }
+	// pl("!true =", !true)
+
+	//String
+	sV1 := "A word"
+	replacer := strings.NewReplacer("A", "Another")
+	sV2 := replacer.Replace((sV1))
+	pl(sV2)
+	pl("length :", len(sV2))
+	pl("Contains Another :", strings.Contains(sV2, "Another"))
+	pl("o index :", strings.Index(sV2, "o"))
+	pl("Replace :", strings.Replace(sV2, "o", "0", -1))
+	sV3 := "\nSome Words\n"
+	sV3 = strings.TrimSpace(sV3)
+	pl("split :", strings.Split("a-b-c-d", "-"))
+	pl("Lower :", strings.ToLower(sV2))
+	pl("Upper :", strings.ToUpper(sV2))
+	pl("Prefix :", strings.HasPrefix("tacocat", "taco"))
+	pl("suffix :", strings.HasSuffix("tacocat", "cat"))
+
 	 
 }
 
