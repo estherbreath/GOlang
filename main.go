5b604f109ea1cc2657658f6be1204300f2432f73
estherbreath <estherojile248@gmail.com>
2023-01-31 07:42:12 +0100

    changes

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	// "log"
 	// "os"
-	// "reflect"
+	"reflect"
+	"strconv"
 )
 
 var pl = fmt.Println
@@ -40,5 +41,24 @@ func main() {
 	cV1 := 1.5
 	cV2 := int(cV1)
 	pl(cV2)
+
+	//String to int
+	cV3 := "5000000"
+	cV4, err := strconv.Atoi(cV3)
+	pl(cV4, err, reflect.TypeOf(cV4))
+
+	//Int to string
+	cV5 := 5000000
+	cV6 := strconv.Itoa(cV5)
+	pl(cV6)
+
+	//String to float 
+	cV7 := "3.14"
+	if cV8, err := strconv.ParseFloat(cV7, 64); err == nil{
+		pl(cV8)
+	}
+
+
+	
 }
 
