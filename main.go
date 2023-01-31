97d50b50d34f9e968be90fcc58dda84ecc683542
estherbreath <estherojile248@gmail.com>
2023-01-31 09:37:58 +0100

    changes

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	// "bufio"
 	"fmt"
-	"strings"
+	// "strings"
+	"unicode/utf8"
 	// "log"
 	// "os"
 	// "reflect"
@@ -76,22 +77,28 @@ func main() {
 	// pl("!true =", !true)
 
 	//String
-	sV1 := "A word"
-	replacer := strings.NewReplacer("A", "Another")
-	sV2 := replacer.Replace((sV1))
-	pl(sV2)
-	pl("length :", len(sV2))
-	pl("Contains Another :", strings.Contains(sV2, "Another"))
-	pl("o index :", strings.Index(sV2, "o"))
-	pl("Replace :", strings.Replace(sV2, "o", "0", -1))
-	sV3 := "\nSome Words\n"
-	sV3 = strings.TrimSpace(sV3)
-	pl("split :", strings.Split("a-b-c-d", "-"))
-	pl("Lower :", strings.ToLower(sV2))
-	pl("Upper :", strings.ToUpper(sV2))
-	pl("Prefix :", strings.HasPrefix("tacocat", "taco"))
-	pl("suffix :", strings.HasSuffix("tacocat", "cat"))
-
-	 
+	// sV1 := "A word"
+	// replacer := strings.NewReplacer("A", "Another")
+	// sV2 := replacer.Replace((sV1))
+	// pl(sV2)
+	// pl("length :", len(sV2))
+	// pl("Contains Another :", strings.Contains(sV2, "Another"))
+	// pl("o index :", strings.Index(sV2, "o"))
+	// pl("Replace :", strings.Replace(sV2, "o", "0", -1))
+	// sV3 := "\nSome Words\n"
+	// sV3 = strings.TrimSpace(sV3)
+	// pl("split :", strings.Split("a-b-c-d", "-"))
+	// pl("Lower :", strings.ToLower(sV2))
+	// pl("Upper :", strings.ToUpper(sV2))
+	// pl("Prefix :", strings.HasPrefix("tacocat", "taco"))
+	// pl("suffix :", strings.HasSuffix("tacocat", "cat"))
+	
+	//Runes(Characters)
+	rStr := "abcdefg"
+	pl("Rune Count :", utf8.RuneCountInString(rStr))
+	for i, runeVal := range rStr{
+		fmt.Printf("%d : %#U : %c\n", i, runeVal, runeVal)
+	}
 }
+
 
