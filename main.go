9cfe3b34e65ecba51275c4c6e3d4093e82045aeb
estherbreath <estherojile248@gmail.com>
2023-02-01 10:39:02 +0100

    changes

--- main.go
+++ main.go
@@ -238,48 +238,48 @@ func main() {
 	// 	} 
 	// }
 
-	// //Slices
-	// aStr1 := "adcde"
-	// rArr := []rune(aStr1)
-	// for _, v := range rArr {
-	// 	fmt.Printf("Rune array : %d\n", v)
-	// }
-	// //Byte array to string
-	// byteArr := []byte{'a', 'b', 'c'}
-	// bStr := string(byteArr[:])
-	// pl("i am a string :", bStr)
+	//Slices
+	aStr1 := "adcde"
+	rArr := []rune(aStr1)
+	for _, v := range rArr {
+		fmt.Printf("Rune array : %d\n", v)
+	}
+	//Byte array to string
+	byteArr := []byte{'a', 'b', 'c'}
+	bStr := string(byteArr[:])
+	pl("i am a string :", bStr)
 	
-	// //var name []datatype
-	// sl1 := make([]string, 6)
-	// sl1[0] = "Society"
-	// sl1[1] = "of"
-	// sl1[2] = "the"
-	// sl1[3] = "simulated"
-	// sl1[4] = "universe"
-	// pl("Slice size :", len(sl1))
-	// for i := 0; i < len(sl1); i++ {
-	// 	pl(sl1[i])
-	// }
-	// for _, x := range sl1 {
-	// 	pl(x)
-	// }
-	// sArr := [5]int{1,2,3,4,5}
-	// sl3 := sArr[0:2]
-	// pl("1st 3 :", sArr[:3])
-	// pl("last 3 :", sArr[2:])
-	// sArr[0] = 10
-	// pl("sl3 :", sl3)
-	// sl3[0] = 1
-	// pl("sArr :", sArr)
+	//var name []datatype
+	sl1 := make([]string, 6)
+	sl1[0] = "Society"
+	sl1[1] = "of"
+	sl1[2] = "the"
+	sl1[3] = "simulated"
+	sl1[4] = "universe"
+	pl("Slice size :", len(sl1))
+	for i := 0; i < len(sl1); i++ {
+		pl(sl1[i])
+	}
+	for _, x := range sl1 {
+		pl(x)
+	}
+	sArr := [5]int{1,2,3,4,5}
+	sl3 := sArr[0:2]
+	pl("1st 3 :", sArr[:3])
+	pl("last 3 :", sArr[2:])
+	sArr[0] = 10
+	pl("sl3 :", sl3)
+	sl3[0] = 1
+	pl("sArr :", sArr)
 
-	// //append value to array
-	// sl3 = append(sl3, 12)
-	// pl("sl3 :", sl3)
-	// pl("sArr :", sArr)
-	// //Empty array
-	// sl4 := make([]string, 6)
-	// pl("sl4 :", sl4)
-	// pl("sl4[0] :", sl4[0])
+	//append value to array
+	sl3 = append(sl3, 12)
+	pl("sl3 :", sl3)
+	pl("sArr :", sArr)
+	//Empty array
+	sl4 := make([]string, 6)
+	pl("sl4 :", sl4)
+	pl("sl4[0] :", sl4[0])
 
 
 	//FUNCTIONS
